b3ee96cc4925ef9b0088f30021e75edc6c13d754
Johan Hall <johan.hall@maltparser.org>
2012-03-19 16:59:32 +0000

    Updated the README file and an example

--- examples/apiexamples/srcex/org/maltparser/examples/TrainingExperiment.java
+++ examples/apiexamples/srcex/org/maltparser/examples/TrainingExperiment.java
@@ -20,9 +20,9 @@ public class TrainingExperiment {
 		try {
 			String trainingDataFile = ".."+File.separator+"data"+File.separator+"talbanken05_train.conll";
 			// Trains the parser model model0.mco and uses the option container 0
-			new MaltParserService(0).runExperiment("-c model0 -i "+trainingDataFile+" -m learn");
+			new MaltParserService(0).runExperiment("-c model0 -i "+trainingDataFile+" -m learn -ne true -nr false");
 			// Trains the parser model model1.mco and uses the option container 1
-			new MaltParserService(1).runExperiment("-c model1 -i "+trainingDataFile+" -m learn");
+			new MaltParserService(1).runExperiment("-c model1 -i "+trainingDataFile+" -m learn -ne true -nr true");
 		} catch (MaltChainedException e) {
 			System.err.println("MaltParser exception : " + e.getMessage());
 		}
